f842006d237a4866a6bb37b591691e7caa39eaec
Vyacheslav Golovin <v.s.golovin@gmail.com>
2023-05-04 22:52:52 +0600

    Add word/guest sweep plots

---
+++ plots/mplstyle.py
@@ -0,0 +1,27 @@
+import matplotlib as mpl
+
+
+MPL_RCPARAMS = {
+    "figure.figsize": [3.5, 2.2],
+    "figure.subplot.bottom": 0.2,
+    "figure.subplot.left": 0.15,
+    "figure.subplot.right": 0.85,
+    "figure.subplot.top": 0.9,
+    "figure.dpi": 300,
+    "legend.frameon": False,
+    "lines.linewidth": 1.0,
+    "font.size": 8.0,
+    "text.usetex": False,
+    "xtick.direction": "in",
+    "xtick.major.size": 2.0,
+    "xtick.minor.size": 1.0,
+    "xtick.top": True,
+    "ytick.direction": "in",
+    "ytick.major.size": 2.0,
+    "ytick.minor.size": 1.0,
+    "ytick.right": True
+}
+
+
+def change_mpl_defaults():
+    mpl.rcParams.update(MPL_RCPARAMS)
--- plots/plot_word_sweep.py
+++ plots/plot_word_sweep.py
@@ -1,13 +1,22 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from mplstyle import change_mpl_defaults
+
 
 df = pd.read_csv("data/word_sweep.csv")
 df_paper = pd.read_csv("data/word_sweep_paper.csv")
+
+change_mpl_defaults()
 
 plt.figure()
 plt.plot(df["T"], df["acc"], label="полученный результат")
 plt.plot(df_paper["T"], df_paper["acc"], label="Seurin et al.")
 plt.legend()
-plt.xlabel(r"Количество спрашиваемых слов $T$")
-plt.ylabel("Точность Guesser")
+plt.xlabel(r"Количество запрашиваемых слов $T$")
+plt.xlim(-1, 21)
+plt.xticks(np.arange(0, 21, 5))
+plt.ylabel(r"Точность Guesser ($K = 5$)")
+plt.ylim(0.48, 1.02)
+plt.yticks(np.arange(0.5, 1.01, 0.1))
 plt.savefig("plots/word_sweep.pdf")
